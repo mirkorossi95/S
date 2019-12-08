fc4cb4b1f467a83792cac880e4c1d44522ccf6fa
mirkorossi95 <mirkorossi1995@gmail.com>
2019-12-08 13:06:27 +0100

    Add files via upload

--- ScriptLauncher.lua
+++ ScriptLauncher.lua
@@ -1,1 +1,4 @@
-c=gg.makeRequest("https://raw.githubusercontent.com/mirkorossi95/Scripts/master/com.gameloft.android.ANMP.GloftA9HM.lua").content if c then pcall(load(c)) end
+c = gg.makeRequest("https://raw.githubusercontent.com/mirkorossi95/Scripts/master/com.gameloft.android.ANMP.GloftA9HM.lua").content
+if c then
+	pcall(load(c))
+end
